5e0e926d82651782cc8d239b8c7289337838852a
Gilang Windu Asmara <gilangwinduasmara2@gmail.com>
2022-09-24 18:49:12 +0700

    feat: problem solved

--- src/App.js
+++ src/App.js
@@ -1,18 +1,53 @@
+import { Card, CardContent, Grid, List } from '@mui/material';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { Box } from '@mui/system';
+import { useState } from 'react';
 import './App.css';
+import Contact from './components/Contact';
+import ContactForm from './components/ContactForm';
+import Header from './components/Header';
 
-// Uncomment untuk memuat daftar kontak
-// import contactsJSON from './data/contacts.json';
+import contactsJSON from './data/contacts.json';
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+    primary: {
+      main: '#1976d2',
+    },
+  },
+});
 const App = () => {
-  // Masukkan Header dan lakukan map untuk Contact ke dalam div App
-  // untuk membuat daftar kontak bisa menggunakan MUI list
-  // https://mui.com/material-ui/react-list/#folder-list
-
-  // Masukkan contacts yang sudah didapat dalam JSON sebagai initial state
-  // Buatlah handler untuk menambahkan kontak baru yang akan dikirim ke ContactForm
-
+  const [contacts, setContacts] = useState(contactsJSON);
+  const addContact = (data) => {
+    console.log(data)
+    setContacts([...contacts, data]);
+  }
   return (
-    <div className="App">
-    </div>
+    <ThemeProvider theme={darkTheme}>
+      <Box sx={{
+        background: 'linear-gradient(to right bottom, #430089, #82ffa1)',
+      }}>
+        <Header />
+        <Grid container spacing={2} padding={8}>
+          <Grid item xs={12} md={6} position={'relative'}>
+            <Box position={'sticky'} top={128}>
+              <ContactForm onSubmit={addContact} />
+            </Box>
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <Card>
+              <CardContent>
+                <List sx={{ width: '100%', maxWidth: 360, color: 'white' }}>
+                  {contacts?.map((contact, index) => (
+                    <Contact data={contact} key={index} />
+                  ))}
+                </List>
+              </CardContent>
+            </Card>
+          </Grid>
+        </Grid>
+      </Box>
+    </ThemeProvider>
   );
 };
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,29 @@
-// Kalian bisa menambahkan CSS di src/components/Header.css
-// atau langsung tambahkan dengan sx={{}}
+import { AppBar, Box, IconButton, Toolbar, Typography } from '@mui/material';
 import React from 'react';
 
-// Header berisi judul dan subtitle
-// Judul: Call a Friend
-// Subtitle: Your friendly contact app
 const Header = () => {
   return (
-    <></>
+    <AppBar position="sticky" color="primary">
+      <Toolbar>
+        <Box
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            justifyContent: 'center',
+            width: '100%',
+            paddingY: 2
+          }}
+        >
+          <Typography variant="h6" noWrap component="div" sx={{ flexGrow: 1 }}>
+            Call a Friend
+          </Typography>
+          <Typography variant="subtitle1" noWrap component="div">
+            Your friendly contact app
+          </Typography>
+        </Box>
+      </Toolbar>
+    </AppBar>
   );
 };
 
